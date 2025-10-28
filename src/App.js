72ec135dbb82915067065e1a58001b1946e0c320
d-detweiler <d.s.detweiler@gmail.com>
2025-10-28 16:24:24 -0700

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,26 @@ export default function App() {
       <header className="App-header">
         <h1>Weather App ☀️</h1>
       </header>
+      <footer>
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/137595-dominika-detweiler"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Dominika Detweiler
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/d-detweiler/weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
