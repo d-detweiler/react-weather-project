e20f7b080e07491bf90b7ed439329f3ec82d3623
d-detweiler <d.s.detweiler@gmail.com>
2025-10-29 21:13:45 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,29 +3,31 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App ☀️</h1>
-      </header>
-      <footer>
-        <p>
-          This project was coded by{" "}
-          <a
-            href="https://www.shecodes.io/graduates/137595-dominika-detweiler"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Dominika Detweiler
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/d-detweiler/weather-app-project"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather App ☀️</h1>
+        </header>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://www.shecodes.io/graduates/137595-dominika-detweiler"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Dominika Detweiler
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/d-detweiler/weather-app-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
