7cd29ce6911beb70dfc48f135d3f5720c7c63130
d-detweiler <d.s.detweiler@gmail.com>
2025-10-30 22:41:54 -0700

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +28,20 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://th.bing.com/th/id/OBWA.v6-106-png-n420?w=72&h=72&c=6&o=6&dpr=1.3&pid=Weather"
-            alt="Mostly Cloudy"
-          />
-          6°C
+          <div className="clearfix">
+            <img
+              src="https://th.bing.com/th/id/OBWA.v6-106-png-n420?w=72&h=72&c=6&o=6&dpr=1.3&pid=Weather"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
